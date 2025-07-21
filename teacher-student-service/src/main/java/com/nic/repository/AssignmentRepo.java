32f771eb35bfb943c1d4fe2cea8b57fbfeab86ec
Mahesh Narayankar <maheshnarayankar19@gmail.com>
2025-07-21 12:20:47 +0530

    Rebased files and folder structure

--- teacher-student-service/src/main/java/com/nic/repository/AssignmentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nic.entity.Assignment;
-
-public interface AssignmentRepo extends JpaRepository<Assignment, Long>{
-   
-}
